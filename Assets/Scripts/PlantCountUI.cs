8032c967f1349cbae548b40fce0addd519ce6e66
CocoaShiba <grin9950@gmail.com>
2025-01-09 21:57:07 -0800

    HW1 Complete
    HW1 Complete

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -8,6 +8,7 @@ public class PlantCountUI : MonoBehaviour
 
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        
+        _plantedText.text = "Seeds Planted: " + seedsPlanted.ToString();
+        _remainingText.text = "Seeds Remaining: " + seedsLeft.ToString();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,12 +13,22 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _plantCountUI.UpdateSeeds(5, 0);
     }
 
     private void Update()
     {
-        
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        input = Vector3.Normalize(input);
+        _playerTransform.Translate(input * _speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _numSeeds > 0)
+        {
+            GameObject instantiatedPlant = Instantiate(_plantPrefab);
+            instantiatedPlant.transform.position = transform.position;
+            _numSeeds--;
+            _plantCountUI.UpdateSeeds(_numSeeds, 5 - _numSeeds);
+        }
     }
 
     public void PlantSeed ()
